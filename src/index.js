a5f8acf80b97b3365754bbcc45504e3d64d3c028
Matthew Wright <matthewbwright@gmail.com>
2018-12-20 19:33:00 -0700

    refactors code for various examples

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function getButtonText() {
-  return 'Click on Me!';
-}
-
 // Create a react component
-const App = function() {
+const App = () => {
+  const buttonText = { text: 'Click Me!' };
+  const labelText = 'Enter name:';
   return (
     <div>
       <label className="label" for="name">
-        Enter name:
+        {labelText}
       </label>
       <input id="name" type="text" />
       <button style={{ backgroundColor: 'blue', color: 'white' }}>
-        {getButtonText()}
+        {buttonText.text}
       </button>
     </div>
   );
